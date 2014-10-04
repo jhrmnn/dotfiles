801f719abcd768876fb5cc885165909a98309b2d
Jan Hermann <jan@janhermann.cz>
2014-10-04 03:30:00 +0200

    color

--- .config/fish/color_scheme.fish
+++ .config/fish/color_scheme.fish
@@ -7,7 +7,7 @@ set -g fish_color_end magenta
 set -g fish_color_error red
 set -g fish_color_escape cyan
 set -g fish_color_history_current cyan
-set -g fish_color_host green
+set -g fish_color_host yellow
 set -g fish_color_match cyan
 set -g fish_color_normal normal
 set -g fish_color_operator cyan
@@ -16,7 +16,7 @@ set -g fish_color_quote green
 set -g fish_color_redirection cyan
 set -g fish_color_search_match --background=magenta
 set -g fish_color_status red
-set -g fish_color_user green
+set -g fish_color_user yellow
 set -g fish_color_valid_path --underline
 set -g fish_pager_color_completion normal
 set -g fish_pager_color_description 555 yellow
