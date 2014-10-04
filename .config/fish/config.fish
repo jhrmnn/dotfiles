7d771adc68be95f75c33312bf4ab26f4d55a6457
Jan Hermann <jan@janhermann.cz>
2014-10-04 12:38:26 +0200

    keyboard

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,9 @@ set -x PATH $HOME/local/bin $PATH
 
 function my_fish_vi_key_bindings
     fish_vi_key_bindings
-    bind -M insert \cc kill-whole-line force-repaint
+    bind \cc kill-whole-line
+    bind -M insert \cc kill-whole-line
+    bind -M insert -m default \e backward-char
 end
 
 set -g fish_key_bindings my_fish_vi_key_bindings
