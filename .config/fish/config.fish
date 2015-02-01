ce4c0bda8f67ad8a41c6a1e8377ab50b7acc2050
Jan Hermann <jan@janhermann.cz>
2015-02-01 22:08:53 +0100

    pythonpath fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,9 @@ source $HOME/.config/fish/color_scheme.fish
 set -x PATH $HOME/local/bin $PATH
 set -x PATH $HOME/bin $PATH
 
+set -x PYTHONPATH $HOME/local/lib $PYTHONPATH
+set -x PYTHONPATH $HOME/bin $PYTHONPATH
+
 set -g fish_key_bindings my_fish_vi_key_bindings
 
 set -x REMOTE (last -n 1 -a ^/dev/null | head -1 | awk '{print $NF}')
