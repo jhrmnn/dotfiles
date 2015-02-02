fab2a96cd1a0d1e774994d2de0974a2008972363
Jan Hermann <jan@janhermann.cz>
2015-02-02 10:15:04 +0100

    fish problem with pythonpath

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,8 @@ source $HOME/.config/fish/color_scheme.fish
 set -x PATH $HOME/local/bin $PATH
 set -x PATH $HOME/bin $PATH
 
-set -x PYTHONPATH $HOME/local/lib $PYTHONPATH
-set -x PYTHONPATH $HOME/bin $PYTHONPATH
+set -x PYTHONPATH $HOME/local/lib:$PYTHONPATH
+set -x PYTHONPATH $HOME/bin:$PYTHONPATH
 
 set -g fish_key_bindings my_fish_vi_key_bindings
 
