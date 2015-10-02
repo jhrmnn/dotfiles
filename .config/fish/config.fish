c514f19bb3042165a3f8855d4ad1f427651428ef
Jan Hermann <jan@janhermann.cz>
2015-10-02 20:10:10 +0200

    separate to local.config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+if test -e ~/.config/fish/local.config.fish
+    . ~/.config/fish/local.config.fish
+end
+
 set -x PATH $HOME/local/bin $PATH
 set -x PATH $HOME/bin $PATH
 set -x LD_LIBRARY_PATH $HOME/local/lib:$HOME/local/lib64 $LD_LIBRARY_PATH
@@ -31,44 +35,4 @@ alias vpn "sudo vpn"
 alias wolfram "/Applications/Mathematica.app/Contents/MacOS/MathKernel"
 alias python "python3"
 
-set -x PATH $PATH "/usr/local/sbin"
-set -x PATH "/Applications/MATLAB.app/bin" $PATH
-set -x PATH $PATH "$HOME/_cache/fhi-aims/bin"
-set -x PATH $PATH "$HOME/code/mbd"
-set -x PYTHONPATH $PYTHONPATH $HOME/_cache/caffe/python
-set -x PYTHONPATH $PYTHONPATH /opt/openbabel/lib/python3.5/site-packages
-
 set -g fish_key_bindings fish_vi_key_bindings
-
-function open -w open
-    if count $argv >/dev/null
-        command open $argv
-    else
-        command open .
-    end
-end
-
-function vim -w vim
-    set -l cmd "vim --servername VIM"
-    if test (count $argv) -eq 1 -a -d "$argv[1]"
-        sh -c "cd $argv[1] && exec $cmd"
-    else
-        eval command $cmd $argv
-    end
-end
-
-function vesta
-    for f in $argv
-        echo -ns \"(realpath $f)\" " "
-    end | xargs open -a VESTA
-end
-
-function cdf
-    set -l target (osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')
-    if test -n $target
-        cd $target
-        pwd
-    else
-        echo 'No Finder window found' ^&1
-    end
-end
