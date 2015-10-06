70d5019c7e2d6bd91133bb3a4cdc356eb6f01d9c
Jan Hermann <jan@janhermann.cz>
2015-10-06 10:12:27 -0400

    fish bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -16,3 +16,5 @@ bind -M insert \cp history-token-search-backward
 bind -M insert \cn history-token-search-forward
 bind -M insert \cu history-search-backward
 bind -M insert \cl forward-char
+bind -M insert \cf 'nextd; and echo; and commandline ""; commandline -f force-repaint'
+bind -M insert \cb 'prevd; and echo; and commandline ""; commandline -f force-repaint'
