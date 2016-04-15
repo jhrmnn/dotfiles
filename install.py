73f411733f9ac488c35046231783b569cc56f09e
Jan Hermann <jh@janhermann.cz>
2016-04-15 14:24:54 +0200

    make python 2.6 compatible

--- install.py
+++ install.py
@@ -3,17 +3,23 @@ from __future__ import print_function
 import subprocess
 import os
 
-files = {p.split(os.path.sep, 1)[1]: os.path.realpath(p)
-         for p in subprocess.check_output('find dotfiles -type f'.split()).split()}
+files = dict((p.split(os.path.sep, 1)[1], os.path.realpath(p))
+             for p in
+             subprocess.Popen('find dotfiles -type f'.split(),
+                              stdout=subprocess.PIPE)
+             .communicate()[0].split())
 if os.path.isfile('.gitignore'):
     with open('.gitignore') as f:
         for p in f:
             p = p.strip()
             if os.path.isabs(p):
                 p = p[1:]
+            if not os.path.isfile(p):
+                continue
             files[p] = os.path.realpath(p)
+home = os.path.realpath(os.environ['HOME'])
 for path, target in files.items():
-    path = os.path.join(os.environ['HOME'], path)
+    path = os.path.join(home, path)
     dirname = os.path.dirname(path)
     if not os.path.isdir(dirname):
         os.makedirs(dirname)
