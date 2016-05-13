090adf2c7d849eb258369ead853e8bbb4637d7d9
Jan Hermann <jh@janhermann.cz>
2016-05-14 00:33:50 +0200

    simplify install

--- install.py
+++ install.py
@@ -1,53 +1,43 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import subprocess
+from subprocess import call, Popen, PIPE
 import os
 import sys
+import stat
 
 
-links = {
-    '.vim/autoload': '.config/nvim/autoload'
-}
-
-
-os.chdir(os.path.dirname(__file__))
-subprocess.call(
-    'curl -ks "https://pub.janhermann.cz/dotfiles/dotfiles/as_targz" | tar -zx',
-    shell=True
-)
-files = dict(
-    (p.split(os.path.sep, 1)[1], os.path.abspath(p))
-    for p in subprocess.Popen(
-        'find dotfiles -type f -o -type l'.split(),
-        stdout=subprocess.PIPE
+def install():
+    files = Popen(
+        'find . -name .git -prune -o -type f ! -name install.*py* -print'.split(),
+        stdout=PIPE
     ).communicate()[0].decode().split()
-)
-files.update(links)
-if os.path.isfile('.gitignore'):
-    with open('.gitignore') as f:
-        for p in f:
-            p = p.strip()
-            if os.path.isabs(p):
-                p = p[1:]
-            if not os.path.isfile(p):
-                continue
-            files[p] = os.path.abspath(p)
-home = os.path.realpath(os.environ['HOME'])
-for path, target in files.items():
-    is_link = path in links
-    path = os.path.join(home, path)
-    dirname = os.path.dirname(path)
-    if not os.path.isdir(dirname):
-        os.makedirs(dirname)
-    if os.path.islink(path):
-        os.unlink(path)
-    if is_link:
-        target = os.path.join(home, target)
-    else:
+    home = os.path.expanduser('~')
+    for path in files:
+        print(path)
+        target = os.path.abspath(path)
+        path = os.path.join(home, path)
+        dirname = os.path.dirname(path)
         target = os.path.relpath(target, dirname)
-    try:
-        os.symlink(target, path)
-    except Exception:
-        print('error: Cannot symlink to {0}'.format(path))
-        sys.exit(1)
-subprocess.call(['fish', '-c', 'source ~/.config/fish/init.fish'])
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        if os.path.islink(path):
+            os.unlink(path)
+        try:
+            os.symlink(target, path)
+        except Exception:
+            print('error: Cannot symlink to {0}'.format(path))
+            sys.exit(1)
+    call(['fish', '-c', 'source ~/.config/fish/init.fish'])
+
+
+if __name__ == '__main__':
+    path = os.path.dirname(__file__)
+    if path:
+        os.chdir(path)
+    call(
+        'curl -ks "https://pub.janhermann.cz/dotfiles/as_targz" | tar -zx --strip-components 1',
+        shell=True
+    )
+    import install as self
+    self.install()
+    os.chmod('install.py', os.stat('install.py').st_mode | stat.S_IEXEC)
