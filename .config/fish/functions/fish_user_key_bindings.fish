9b89e2a7a88a83ab358a69f05ea24014198a6b8e
Jan Hermann <jh@janhermann.cz>
2016-05-14 00:34:23 +0200

    move everything up

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,11 +2,11 @@ if functions -q fzf_key_bindings
     fzf_key_bindings
 end
 
-function new-line
+function cancel_commandline
     set -l cmd (commandline)
-    if [ -n "$cmd" ]
+    if [ -n $cmd ]
         commandline -C 1000000
-        echo -n (set_color -o brgrey)'^C'(set_color normal)
+        echo -n (set_color brgrey -o)'^C'(set_color normal)(tput el)
     end
     for i in (seq (commandline -L))
         echo ''
@@ -15,8 +15,8 @@ function new-line
     commandline -f repaint
 end
 
-bind -M insert \cc new-line
-bind -M default -m insert \cc new-line
+bind -M insert \cc cancel_commandline
+bind -m insert \cc cancel_commandline
 bind -M insert \ej history-token-search-forward
 bind -M insert \ek history-token-search-backward
 bind -M insert \el nextd-or-forward-word
