2df56d8ddf526ccba17d41589312df94b9b30f78
Jan Hermann <jh@janhermann.cz>
2016-05-15 01:40:04 +0200

    custom colors in fish

--- dotfiles/.config/fish/init.fish
+++ dotfiles/.config/fish/init.fish
@@ -2,11 +2,31 @@ set -U fish_key_bindings fish_vi_key_bindings
 set -U fish_prompt_pwd_dir_length 2
 set -U fish_fzf_path (dirname (dirname (which fzf)))
 
-set fish_color_comment brgrey
-
 set -eU fish_greeting
-set -eU fish_user_abbreviations
 
+set -U fish_color_normal normal
+set -U fish_color_error -o red
+set -U fish_color_command -o brwhite
+set -U fish_color_param blue
+set -U fish_color_escape magenta
+set -U fish_color_end -o brwhite
+set -U fish_color_quote green
+set -U fish_color_operator yellow
+set -U fish_color_autosuggestion bryellow
+set -U fish_color_comment brgrey
+set -U fish_color_redirection magenta
+set -U fish_color_history_current green
+set -U fish_color_match --background=magenta
+set -U fish_color_search_match --background=bryellow
+set -U fish_color_selection --background=bryellow
+set -U fish_color_valid_path --underline
+
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description bryellow
+set -U fish_pager_color_prefix blue
+set -U fish_pager_color_progress cyan
+
+set -eU fish_user_abbreviations
 abbr vi vim
 abbr ll ls -l
 abbr la ls -la
