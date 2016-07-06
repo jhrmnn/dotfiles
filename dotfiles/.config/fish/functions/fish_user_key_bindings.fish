83bb08e4df9d9899fd1a5c4f5b4af68aa682f938
Jan Hermann <jh@janhermann.cz>
2016-07-06 12:22:59 -0400

    add fish shortcut for browsing directory history

--- dotfiles/.config/fish/functions/fish_user_key_bindings.fish
+++ dotfiles/.config/fish/functions/fish_user_key_bindings.fish
@@ -15,8 +15,22 @@ function cancel_commandline
     commandline -f repaint
 end
 
+function fzf_dir_history
+    if test -n "$dirprev"
+        set -l dir
+        dirh | sed '$ d' | fzf --ansi -n 2.. +s --tac | \
+        sed 's;^ *\([0-9]\{1,\}) *\)\{0,\}/;/;' | read dir
+        if test -n "$dir"
+            cd $dir
+            commandline ''
+        end
+        commandline -f repaint
+    end
+end function
+
 bind -M insert \cc cancel_commandline
 bind -m insert \cc cancel_commandline
+bind -M insert \ch fzf_dir_history
 bind -M insert \ej history-token-search-forward
 bind -M insert \ek history-token-search-backward
 bind -M insert \el nextd-or-forward-word
