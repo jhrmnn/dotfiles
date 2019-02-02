77cff6715000c7d19e41904c52ab7db932ec1641
Jan Hermann <dev@janhermann.cz>
2019-02-02 17:53:16 +0100

    update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,33 +1,45 @@
 function fish_prompt
-    set -l _status $status
-    if [ -n "$POETRY_ACTIVE" ]
-        echo -ns "(" (basename $VIRTUAL_ENV) ") "
-    end
-    echo -ns (date "+%H:%M")
+    set _status $status
+
     set_color normal
+
+    echo -ns (date "+%H:%M")
+
     if [ -n "$SSH_CLIENT" -o -n "$SSH_CLIENT2" ]
         if not set -q __fish_prompt_hostname
             set -g __fish_prompt_hostname (prompt_hostname)
         end
-        echo -ns " " (set_color brred) $USER \
-        (set_color brcyan) "@" (set_color brred) $__fish_prompt_hostname
+        echo -ns " " (set_color brred) $USER (set_color brcyan) "@" (set_color brred) $__fish_prompt_hostname
     end
-    echo -ns " " (set_color -o cyan) (prompt_pwd)
-    if [ $PROMPT_PREFIX ]
-        echo -ns (set_color -o magenta) " {$PROMPT_PREFIX}"
+
+    if [ -n "$PROMPT_PATH_ROOT" ]
+        set path_root $PROMPT_PATH_ROOT
+        set root_symbol "{"(basename $PROMPT_PATH_ROOT)"}"
+    else
+        set path_root ~
+        set root_symbol '~'
     end
+    set mypwd (string replace -r '^'"$path_root"'($|/)' '!!$1' $PWD)
+    set mypwd (string replace -ar '(\.?[^/]{2})[^/]*/' '$1/' $mypwd)
+    set mypwd (string replace -r '^!!($|/)' (set_color -o green)"$root_symbol"(set_color -o cyan)'$1' $mypwd)
+    echo -ns " " (set_color -o cyan) $mypwd
+
     if git rev-parse --is-inside-work-tree >/dev/null ^&1
         set_color -o (if git status --porcelain | egrep . >/dev/null ^&1; echo yellow; else; echo green; end)
         __fish_git_prompt
     end
-    set -l njobs (jobs | wc -l | string trim)
+
+    set njobs (jobs | wc -l | string trim)
     if [ $njobs -gt 0 ]
         echo -ns (set_color -o blue) " $njobs"
     end
+
     if [ $_status -ne 0 ]
         echo -ns (set_color -o red) " [$_status]"
     end
+
     echo -ns (set_color normal)
+
     switch $fish_bind_mode
         case visual
             echo -ns " 🐠 "
