143b6e1c4f93990ec5c335068b97775dda7e6923
Jan Hermann <dev@janhermann.cz>
2019-02-04 00:52:40 +0100

    update

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,6 @@
 function fish_right_prompt
     if [ -n "$PROMPT_RIGHT" ]
-        echo -ns (set_color -o magenta) " $PROMPT_RIGHT"
+        echo -ns (set_color -o magenta) " $PROMPT_RIGHT" (set_color normal)
     end
 end
 
