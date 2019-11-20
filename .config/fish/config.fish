9cf3413de3d357f422620c9445305a30c4f1fa63
Jan Hermann <dev@jan.hermann.name>
2019-11-20 21:33:00 +0100

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,11 @@ if status --is-login
     set -x SHELL (which fish)
 end
 
+alias vim 'nvim'
+alias vimdiff 'nvim -d'
+
 if [ -f ~/.config/fish/local.config.fish ]
     source ~/.config/fish/local.config.fish
 end
+
+direnv hook fish | source
